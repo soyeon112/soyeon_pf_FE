511153819078ca812f42e11c3be81cbcfd43bbfe
soyeon <kimsoyeon1112@gmail.com>
2023-11-04 16:11:29 +0900

    [fix] proxy 관련 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
+  // app.use(
+  //   "/",
+  //   createProxyMiddleware({
+  //     target: process.env.REACT_APP_PROXY_LOCAL,
+  //     changeOrigin: true,
+  //   })
+  // );
   app.use(
-    "/",
-    createProxyMiddleware({
-      target: process.env.REACT_APP_PROXY_LOCAL,
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/api",
+    process.env.REACT_APP_API,
     createProxyMiddleware({
       target: process.env.REACT_APP_PROXY,
       changeOrigin: true,
