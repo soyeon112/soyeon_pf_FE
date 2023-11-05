b87726d35c0881001707dcad8d8bd81ba646021b
soyeon <kimsoyeon1112@gmail.com>
2023-11-05 20:11:58 +0900

    [fix] 프록시 local > cloudtype 으로 코드 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     process.env.REACT_APP_API,
     createProxyMiddleware({
-      target: process.env.REACT_APP_PROXY_LOCAL,
+      target: process.env.REACT_APP_PROXY,
       changeOrigin: true,
     })
   );
